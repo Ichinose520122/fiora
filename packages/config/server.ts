acc4fdddb2e1726faa02f4fc776d63b1a0c607c0
Ichinose520122 <ichinose520122@gmail.com>
2026-07-27 19:58:12 +0800

    Update server.ts

--- packages/config/server.ts
+++ packages/config/server.ts
@@ -9,6 +9,10 @@ export default {
     // service port
     port: env.Port ? parseInt(env.Port, 10) : 9200,
 
+    maxHttpBufferSize: env.MaxHttpBufferSize
+    ? parseInt(env.MaxHttpBufferSize, 10)
+    : 32 * 1024 * 1024,
+
     // mongodb address
     database: env.Database || 'mongodb://localhost:27017/fiora',
 
